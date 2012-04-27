4ba70fb76ad128ccfff23783b98bdf6d9c34a55a
Denis Krjuchkov <info@x-tensive.com>
2012-04-27 15:22:08 +0600

    Added support for building zip packages, various other improvements

--- Sources/ProductInfo.cs
+++ Sources/ProductInfo.cs
@@ -1,15 +1,15 @@
-using System.Reflection;
+ï»¿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("DataObjects.Net Extensions")]
 [assembly: AssemblyCompany("Xtensive LLC, Alexander Ovchinnikov")]
-[assembly: AssemblyCopyright("© 2003-2012 Xtensive LLC, © 2011-2012 Alexander Ovchinnikov")]
+[assembly: AssemblyCopyright("Â© 2003-2012 Xtensive LLC, Â© 2011-2012 Alexander Ovchinnikov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyFileVersion("4.5.0.1")]
+[assembly: AssemblyVersion("${Version}.0")]
+[assembly: AssemblyFileVersion("${Version}.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
