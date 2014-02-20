3e7bb243c33d1f7c5b4d1771dd64035b7c192f8a
Alexey Kulakov <info@x-tensive.com>
2014-02-20 20:39:29 +0600

    Xtensive.Orm.Logging.log4net
    - Added log.
    - Added provider.
    - Added test.
    - Added ReadMe file.

---
+++ Sources/Xtensive.Orm.Logging.log4net/LogProvider.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2014 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2014.02.20
+
+namespace Xtensive.Orm.Logging.log4net
+{
+  public class LogProvider : Logging.LogProvider
+  {
+    public override BaseLog GetLog(string logName)
+    {
+      return new Log(logName);
+    }
+  }
+}
