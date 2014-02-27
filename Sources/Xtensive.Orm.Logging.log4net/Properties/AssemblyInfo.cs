7be7787431154c224ba0634a80a3d78202e3f44d
Alexey Kulakov <info@x-tensive.com>
2014-02-27 17:30:56 +0600

    Fixed Xtensive.Orm.Logging.log4net
    - Added nuget package file.
    - Fixed AssemblyInfo.
    - Fixed project files

--- Sources/Xtensive.Orm.Logging.log4net/Properties/AssemblyInfo.cs
+++ Sources/Xtensive.Orm.Logging.log4net/Properties/AssemblyInfo.cs
@@ -1,36 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Xtensive.Orm.Logging.log4net")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xtensive.Orm.Logging.log4net")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c6cfb5e1-5ecc-4021-a158-60fafbac9d83")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("DataObjects.Net logging provider for log4net")]
