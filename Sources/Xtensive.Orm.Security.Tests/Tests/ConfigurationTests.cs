5905c152dfd0fb6abbbd60fbe161b460a15c9bbf
Alexey Kulakov <info@x-tensive.com>
2018-10-12 20:23:02 +0500

    Fixed related to Configuration loading test

--- Sources/Xtensive.Orm.Security.Tests/Tests/ConfigurationTests.cs
+++ Sources/Xtensive.Orm.Security.Tests/Tests/ConfigurationTests.cs
@@ -4,26 +4,25 @@
 // Created by: Dmitri Maximov
 // Created:    2011.06.07
 
-using System;
-using System.Configuration;
 using NUnit.Framework;
+using TestCommon;
 using Xtensive.Orm.Security.Configuration;
 
 namespace Xtensive.Orm.Security.Tests
 {
   [TestFixture]
-  public class ConfigurationTests
+  public class ConfigurationTests : HasConfigurationAccessTest
   {
     [Test]
     public void HashingServiceNameTest()
     {
-      var section = (Configuration.ConfigurationSection) ConfigurationManager.GetSection("Xtensive.Orm.Security.WithName");
+      var section = (Configuration.ConfigurationSection) Configuration.GetSection("Xtensive.Orm.Security.WithName");
       Assert.That(section, Is.Not.Null);
       Assert.That(section.HashingService, Is.Not.Null);
       Assert.That(section.HashingService.Name, Is.Not.Null);
       Assert.That(section.HashingService.Name, Is.EqualTo("md5"));
 
-      var config = SecurityConfiguration.Load("Xtensive.Orm.Security.WithName");
+      var config = SecurityConfiguration.Load(Configuration, "Xtensive.Orm.Security.WithName");
       Assert.That(config, Is.Not.Null);
       Assert.That(config.HashingServiceName, Is.EqualTo("md5"));
     }
@@ -31,14 +30,12 @@ namespace Xtensive.Orm.Security.Tests
     [Test]
     public void HashingServiceEmptyTest()
     {
-      var section = (Configuration.ConfigurationSection) ConfigurationManager.GetSection("Xtensive.Orm.Security.WithoutName");
+      var section = (Configuration.ConfigurationSection) Configuration.GetSection("Xtensive.Orm.Security.WithoutName");
       Assert.That(section, Is.Not.Null);
       Assert.That(section.HashingService, Is.Not.Null);
       Assert.That(section.HashingService.Name, Is.Null.Or.Empty);
 
-      var config = SecurityConfiguration.Load("Xtensive.Orm.Security.WithoutName");
-      //Assert.IsNotNull(config);
-      //Assert.AreEqual("plain", config.HashingServiceName);
+      var config = SecurityConfiguration.Load(Configuration, "Xtensive.Orm.Security.WithoutName");
       Assert.That(config, Is.Not.Null);
       Assert.That(config.HashingServiceName, Is.EqualTo("plain"));
     }
@@ -46,12 +43,12 @@ namespace Xtensive.Orm.Security.Tests
     [Test]
     public void HashingServiceAbsentTest()
     {
-      var section = (Configuration.ConfigurationSection) ConfigurationManager.GetSection("Xtensive.Orm.Security.Empty");
+      var section = (Configuration.ConfigurationSection) Configuration.GetSection("Xtensive.Orm.Security.Empty");
       Assert.That(section, Is.Not.Null);
       Assert.That(section.HashingService, Is.Not.Null);
       Assert.That(section.HashingService.Name, Is.Null.Or.Empty);
 
-      var config = SecurityConfiguration.Load("Xtensive.Orm.Security.Empty");
+      var config = SecurityConfiguration.Load(Configuration, "Xtensive.Orm.Security.Empty");
       Assert.That(config, Is.Not.Null);
       Assert.That(config.HashingServiceName, Is.EqualTo("plain"));
     }
@@ -59,25 +56,24 @@ namespace Xtensive.Orm.Security.Tests
     [Test]
     public void HashingServiceNoConfigTest()
     {
-      var section = (Configuration.ConfigurationSection) ConfigurationManager.GetSection("Xtensive.Orm.Security.XXX");
-      Assert.That(section, Is.Not.Null);
-      Assert.IsNull(section);
+      var section = (Configuration.ConfigurationSection) Configuration.GetSection("Xtensive.Orm.Security.XXX");
+      Assert.That(section, Is.Null);
 
-      var config = SecurityConfiguration.Load("Xtensive.Orm.Security.XXX");
+      var config = SecurityConfiguration.Load(Configuration, "Xtensive.Orm.Security.XXX");
       Assert.That(config, Is.Not.Null);
-      Assert.That(config, Is.EqualTo("plain"));
+      Assert.That(config.HashingServiceName, Is.EqualTo("plain"));
     }
 
     [Test]
     public void HashingServiceDefaultTest()
     {
-      var section = (Configuration.ConfigurationSection) ConfigurationManager.GetSection("Xtensive.Orm.Security");
+      var section = (Configuration.ConfigurationSection) Configuration.GetSection("Xtensive.Orm.Security");
       Assert.That(section, Is.Not.Null);
       Assert.That(section.HashingService, Is.Not.Null);
       Assert.That(section.HashingService.Name, Is.Not.Null.Or.Empty);
       Assert.That(section.HashingService.Name, Is.EqualTo("sha1"));
 
-      var config = SecurityConfiguration.Load();
+      var config = SecurityConfiguration.Load(Configuration);
       Assert.That(config, Is.Not.Null);
       Assert.That(config.HashingServiceName, Is.EqualTo("sha1"));
     }
