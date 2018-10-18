9561d016b57819ed3e756c438254c4bf5947853a
Alexey Kulakov <info@x-tensive.com>
2018-10-18 21:26:54 +0500

    Added extensions file

---
+++ Sources/Xtensive.Orm.Web/AplicationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xtensive.Core;
+using Microsoft.AspNetCore.Builder;
+
+namespace Xtensive.Orm.Web
+{
+  public static class AplicationBuilderExtensions
+  {
+    public static IApplicationBuilder UseSessionManager(this IApplicationBuilder builder)
+    {
+      return builder.UseMiddleware<SessionManagerMiddleware>();
+    }
+
+    public static IApplicationBuilder UseSessionManager(this IApplicationBuilder builder, Func<Pair<Session, System.IDisposable>> sessionProvider)
+    {
+      SessionManagerMiddleware.SessionProvider = sessionProvider;
+      return builder.UseMiddleware<SessionManagerMiddleware>();
+    }
+  }
+}
